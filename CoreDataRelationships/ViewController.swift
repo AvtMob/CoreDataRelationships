296e34b617eab7e9250a40287c10ae94ab015c9b
Avtar <dhaliwal.avtar1@gmail.com>
2018-02-27 22:53:49 +0530

    Initial Commit

---
+++ CoreDataRelationships/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataRelationships
+//
+//  Created by Avtar Singh on 27/02/18.
+//  Copyright © 2018 Avtar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
